db52802ffd4f76a2bf06e8551d091e73872c62fc
YANG,ZHI-XIAN <91250092+zhixian0123@users.noreply.github.com>
2021-10-14 17:37:55 +0800

    Add files via upload

---
+++ week05/week05-4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    if(-2) printf("-2成立\n");
+    if(-1) printf("-1成立\n");
+    if(0) printf("0成立\n");
+    if(1) printf("1成立\n");
+    if(2) printf("2成立\n");
+    if(3) printf("3成立\n");
+    if(9999) printf("9999成立\n");
+}
