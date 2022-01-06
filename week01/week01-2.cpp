2b3c28084b67fa8d6ada78a850265adc85b8c47d
zhixian0123 <10160765@me.mcu.edu.tw>
2022-01-06 17:01:58 +0800

    add week01

---
+++ week01/week01-2.cpp
@@ -0,0 +1,12 @@
+///(1) File-New-EmptyFile Ctrl-Shift-N 開新檔
+///(2) File-SaveAs另存新檔, week01-1.cpp 變彩色
+///小心英文輸入法 # 大寫3 (＃中文太大，錯誤)
+#include <stdio.h> ///小心, 角括 大寫,.
+///不是   stduio.h 是 stdio.h
+
+int main() ///圓括 大寫9,0
+{
+    printf( "Hello World\n" );
+}         ///雙引號 大寫 Enter旁
+///大括號,
+///Ctrl-滑鼠滾輪,字放大
